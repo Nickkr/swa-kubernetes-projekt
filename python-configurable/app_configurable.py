8850e92285ac355f613cf6ed596ff26dbd6e8e4d
Dominik Roß <rodo1015@hs-karlsruhe.de>
2021-05-17 12:50:11 +0200

    Implemented multiprocessing

--- python-configurable/app_configurable.py
+++ python-configurable/app_configurable.py
@@ -1,21 +1,57 @@
+from concurrent.futures import process
 import sys
 import time
+from multiprocessing import Process
 
-counter = 0
+
+def endless_loop(cpu_load, frequency):
+    sleep_duration = frequency * (1 - cpu_load)
+    lastTime = time.perf_counter()
+
+    counter = 0
+
+    while True:
+        counter = counter + 1
+        if time.perf_counter() - lastTime >= frequency:
+            #print(lastTime-time.perf_counter(), " sleeping...")
+            time.sleep(sleep_duration)
+            #print(lastTime-time.perf_counter(), " ended sleeping...")
+            lastTime = time.perf_counter()
+
+        # You can comment in the print statements for debugging purposes
+        # In production, it will pollute your memory
+
+# Source:https://www.geeksforgeeks.org/python-program-for-sieve-of-eratosthenes/
+def sieve_of_eratosthenes(n):
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+          
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p] == True):
+              
+            # Update all multiples of p
+            for i in range(p * 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    prime[0]= False
+    prime[1]= False
 
 cpu_load = float(sys.argv[1]) #e.g 0.8 (%)
 frequency = float(sys.argv[2]) #e.g 2 seconds
-#--> in this case the while-loop should sleep for 0.4s each 2 seconds
-sleep_duration = frequency * (1 - cpu_load)
-lastTime = time.perf_counter()
+cores = int(sys.argv[3])
 
-while True:
-    counter = counter + 1
-    if time.perf_counter() - lastTime >= frequency:
-        #print(lastTime-time.perf_counter(), " sleeping...")
-        time.sleep(sleep_duration)
-        #print(lastTime-time.perf_counter(), " ended sleeping...")
-        lastTime = time.perf_counter()
+processes = list()
+for index in range(cores):
+    x = Process(target=endless_loop, args=(cpu_load, frequency,))
+    #x = Process(target=SieveOfEratosthenes, args=( 1000000000,))
+    processes.append(x)
+    x.start()
 
-# You can comment in the print statements for debugging purposes
-# In production, it will pollute your memory
+# Wait for threads to end
+for index in range(cores):
+    processes[index].join()
+
+
+
+
